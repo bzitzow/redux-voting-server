0fbc36a8d7ba121b8e973d25f845a1cbaaa7d81c
Brian Zitzow <brzitzow@streamsend.com>
2016-06-14 14:55:04 -0700

    Start the vote. Add the #next method and build a new state tree.

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -22,7 +21,21 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
+  });
 
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
   });
 
 });
